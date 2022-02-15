65bde8d59161d53b5bbee3796e4214f8c055dec0
Riuqlav <valquirsolza.vs@gmail.com>
2022-02-15 21:13:15 +0100

    Fixing use effect

--- src/Pages/AllMeetups.js
+++ src/Pages/AllMeetups.js
@@ -13,10 +13,20 @@ function AllMeetupsPage() {
         return response.json();
       })
       .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+
         setIsLoading(false);
-        setLoadedMeetups(data);
+        setLoadedMeetups(meetups);
       });
-  }, [third]);
+  }, []);
 
   if (isLoading) {
     return (
